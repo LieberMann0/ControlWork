1399b6798c64b54b84457ee758a2c03db8b1aaf1
Администратор <lieber.mann.0@gmail.com>
2022-01-03 00:59:39 +0300

    Create PrintArray method

--- dev1/Program.cs
+++ dev1/Program.cs
@@ -2,6 +2,17 @@
    Первоначальный массив можно ввести с клавиатуры, либо сгенерировать случайным образом.
    При решении не рекомендуется пользоваться коллекциями, лучше обойтись исключительно массивами.*/
 
+void PrintArray(int[] array)
+{
+    int count = array.Length;
+    int IndArray = 0;
+    while (IndArray < count)
+    {
+        Console.Write(array[IndArray]+"  ");
+        IndArray++;
+    }
+}
+
 Random Rnd = new Random();
 int Ind = Rnd.Next(10, 20);
 int[] ArrayNumbers = new int[Ind];
@@ -12,4 +23,6 @@ for (int i = 0; i < Ind; i++)
     ArrayNumbers[i] = Rnd.Next(-10, 10);
 }
 
+PrintArray(ArrayNumbers);
+Console.WriteLine();
 
