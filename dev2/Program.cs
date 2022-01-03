a3a0821636be950b4825a23a69ed73f4306466a0
Администратор <lieber.mann.0@gmail.com>
2022-01-03 06:23:26 +0300

    PrintArray method

--- dev2/Program.cs
+++ dev2/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Написать программу, которая из имеющегося массива целых чисел
+// формирует массив из чисел, больших 8.
+
+void PrintArray(int[] array)
+{
+    int count = array.Length;
+    int IndArray = 0;
+    while (IndArray < count)
+    {
+        Console.Write(array[IndArray]+"  ");
+        IndArray++;
+    }
+}
+
